0639af9cead168b5f701ae076b24df322793247b
jcaste05 <124076427+jcaste05@users.noreply.github.com>
2023-04-22 13:15:56 +0200

    Update ej_5_cliente_temporizador.py

--- ej_5_cliente_temporizador.py
+++ ej_5_cliente_temporizador.py
@@ -5,14 +5,14 @@ import sys
 
 def on_connect(client, rc):
     print("Conectado con código de resultado: " + str(rc))
-    client.subscribe('temporizador')
+    client.subscribe('temporizador') #Nos suscribiríamos a un canal que se llame temporizador del cual recibiremos el topic que se quiere publicar, la espera antes de ser publicado y el mensaje
 
 def on_message(client, userdata, msg):
     payload_str = msg.payload.decode('utf-8')
     topic, espera, mensaje = payload_str.split(';')
-    t_espera = int(espera)
-    sleep(t_espera)
-    client.publish(topic,  mensaje)
+    t_espera = int(espera) 
+    sleep(t_espera) #Esperamos el tiempo indicado
+    client.publish(topic,  mensaje) #En el topic indicado publicamos el mensaje que recibimos
 
 # función principal
 def main(hostname):
