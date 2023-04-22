2f30150f318d66426cc6b7b14c9afa80a3bd9ba3
jcaste05 <124076427+jcaste05@users.noreply.github.com>
2023-04-22 13:24:23 +0200

    Update ej_6_cliente_cadena.py

--- ej_6_cliente_cadena.py
+++ ej_6_cliente_cadena.py
@@ -5,10 +5,10 @@ from paho.mqtt.client import Client
 import sys
 from time import sleep
 
-values = []
+values = [] #Lista para almacenar los valores de humedad
 
-# Función para determinar si un número es primo
-def is_prime(n):
+# Función para saber si es primo un número entero positivo
+def primo(n):
     if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
@@ -16,37 +16,38 @@ def is_prime(n):
             return False
     return True
 
-# Función para calcular el valor medio de una lista de valores
-def calculate_mean(values):
+# Función para hallar la media
+def media(values):
     if len(values) == 0:
         return 0
     return sum(values) / len(values)
 
 def on_connect(client, rc):
     print("Conectado con código de resultado: " + str(rc))
-    client.subscribe("numbers")
+    client.subscribe("numbers") #Primero nos conectamos al topic 'numbers'
     
-# Función que se ejecutará cuando se reciba un mensaje en el topic "numbers"
+# Función que se ejecutará cuando se reciba un mensaje del topic "numbers"
 def on_message_numbers(client, userdata, message):
     number = int(message.payload.decode())
     print(f"Número recibido: {number}")
-    if is_prime(number):
-        print("Número primo, leyendo humedad durante 5 segundos...")
+    if primo(number): #Si es primo nos suscribimos al canal humedad durante 5 segundos
+        print("Número primo, leyendo humedad durante 7 segundos...")
         values.clear()
         client.subscribe("humidity")
-        time.sleep(5) # Esperamos 5 segundos
+        time.sleep(7) # Esperamos 7 segundos
         client.unsubscribe("humidity")
 
-# Función que se ejecutará cuando se reciba un mensaje en el topic "humidity"
+# Función que se ejecutará cuando se reciba un mensaje del topic "humidity"
 def on_message_humidity(client, userdata, message):
     humidity = float(message.payload.decode())
     print(f"Received humidity: {humidity}")
-    values.append(humidity)
-    if len(values) >= 5:
+    values.append(humidity) #Añadimos el valor a la lista values de humedades
+    mean = 0
+    if len(values) >= 5: #Si ya hemos leído 5 valores hallamos la media y vaciamos values
         mean = calculate_mean(values)
-        print(f"Media humedad en 5 segundos: {mean}")
+        print(f"Media humedad últimos 5 valores: {mean}")
         values.clear()
-    if mean > 20:
+    if mean > 20: #Si superamos la humedad umbral se publica en temporizador para que se publique en 5 segundos el mensaje superada en el topic humedad_umbral
         print('Humdedad umbral superada, poniendo temporizador...')
         client.publish('temporizador', 'humedad_umbral;5;superada')
         
